191e13ec72ee14141811134cde4fdb4761a2c3d2
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2021-01-07 19:01:05 +0200

    adding script with a function for creating and adding li and a elements to the ol list, adding an array with items for the list, adding some folders

---
+++ js/main.js
@@ -0,0 +1,52 @@
+
+// defining the function
+const makeList =()=> {
+
+    // the list of links
+    const links = [
+        {
+          label: "Week1 notes",
+          url: "week1/index.html"
+        },
+        {
+            label: "Week2 notes",
+            url: "week2/index.html"
+          },
+        {
+            label: "Week3 notes",
+            url: "week3/index.html"
+          }
+      ]
+
+    
+      // Reading the list of links from the array
+    links.forEach(v => {
+
+        // creating a new li element
+        let li = document.createElement("li");
+
+        // creating a new a element
+        let a = document.createElement("a");
+
+        // creating a text node
+        let text = document.createTextNode(v.label);
+
+        // apending the text node to the a element
+        a.appendChild(text);
+
+        // setting the href attribute for the newly created a element
+        a.setAttribute("href", v.url);
+
+        // appending the a element to the li element
+        li.appendChild(a);
+
+        // Selecting the ol element from the root index.html page
+        let list = document.getElementById("list");
+
+        // appending the li element to the selected list
+        list.appendChild(li);
+    })
+}
+
+// running the function
+makeList();
