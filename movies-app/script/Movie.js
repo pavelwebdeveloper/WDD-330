db40dfd2deae3602b0e6a542d9a46f4df0754809
Pavel Shvets <38735112+pavelwebdeveloper@users.noreply.github.com>
2021-03-29 23:22:19 +0300

    movie details
    adding logic to get movie details

--- movies-app/script/Movie.js
+++ movies-app/script/Movie.js
@@ -6,15 +6,16 @@ export default class Movie {
    
     this._movies = [];
     
-
-    this.baseUrl = 'http://www.omdbapi.com/?apikey=6c3a52ae&s=Alice in Wonderland';
+    this.baseUrl = 'http://www.omdbapi.com/?';
+    this.urlToGetSomeMovies = 'http://www.omdbapi.com/?apikey=6c3a52ae&s=Alice in Wonderland';
     //this.baseUrl = 'http://www.omdbapi.com/?apikey=6c3a52ae&s=Winnie the Pooh';
+    this.urlToGetMovieDetails = 'http://www.omdbapi.com/?i=tt3896198&apikey=6c3a52ae';
     
   }
   async getMovies() {
    
      
-      await getData(this.baseUrl).then(data => {
+      await getData(this.baseUrl + 'apikey=6c3a52ae&s=Alice in Wonderland').then(data => {
         console.log(data.Search);
         this._movies = data.Search;
       });
@@ -22,5 +23,16 @@ export default class Movie {
     return this._movies;
   }
 
+
+  async getMovieDetailsById(id) { 
+    const movieDetails = await getData(`${this.baseUrl}i=${id}&apikey=6c3a52ae`)/*.then(data => {
+      console.log(data);
+      return data;
+    });*/
+    return movieDetails;
+    // filter this._quakes for the record identified by id and return it
+    //const movieDetails = await this._quakes.then(result => {return result.features;})
+    //return features.filter(item => item.id === id)[0];
+  }
   
 }
--- movies-app/script/MoviesController.js
+++ movies-app/script/MoviesController.js
@@ -21,17 +21,45 @@ export default class QuakesController {
     this.parentElement = document.querySelector(this.parent);
     
     console.log(this.parentElement);
-    this.movies = await this.movies.getMovies();
+    const movieList = await this.movies.getMovies();
     //console.log(await this.movies.getMovies());
     console.log(this.movies); 
     this.parentElement.innerHTML = 'Loading...'; 
-    this.moviesView.renderMoviesList(this.movies, this.parentElement);  
-    this.parentElement.addEventListener('click', e => {
-      console.log("child");
-      console.log(e.target.value);   
     
+    this.moviesView.renderMoviesList(movieList, this.parentElement);  
+    this.parentElement.addEventListener('click', e => {
+      //console.log("child");
+      //console.log(e.target.parentElement.getAttribute('data-id'));    
+      this.getMovieDetails(e.target.parentElement.getAttribute('data-id'));
   });
   }
+
+  async getMovieDetails(movieId) {
+    // get the details for the quakeId provided from the model, then send them to the view 
+    //to be displayed 
+    let element = document.querySelector(`[data-id="${movieId}"]`); 
+    console.log(element);
+    console.log(this.movies);
+    
+    
+    const movieDetails = await this.movies.getMovieDetailsById(movieId);
+
+    
+    
+    
+    console.log("movieDetails &&&&&&&&&&&&&&");
+    console.log(movieDetails);
+    
+    this.moviesView.renderMovie(properties, element);
+    let elementStorage = element;
+      document.querySelector('[id="quakeList"]').innerHTML = ''; 
+      document.querySelector('[id="quakeList"]').appendChild(elementStorage);
+      console.log("THE CLASSES:    ");
+      console.log(document.getElementById("backToListButton"));
+      document.getElementById("backToListButton").classList.remove("invisible");
+   
+  }
+  
  
    
 }   
